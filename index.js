03f83633247473f6c23da674ac4afb6385e807db
komald4 <komald.4@gmail.com>
2020-03-12 12:03:34 -0400

    Komal David

--- index.js
+++ index.js
@@ -3,4 +3,58 @@ document.addEventListener("DOMContentLoaded", function(){
   renderBot(currentPosition)
 
   // ADD CODE HERE!
+
+  document.addEventListener('keydown', function(e) {
+    let move_bot
+
+    let ul = document.getElementById("moves-container");
+
+    if (e.key === "ArrowLeft") {
+      move_bot = "left"
+    }
+    else if (e.key === "ArrowRight"){
+      move_bot = "right"
+    }
+    else if (e.key === "ArrowUp"){
+      move_bot = "up"
+    }
+    else if (e.key === "ArrowDown"){
+      move_bot = "down"
+    }
+
+    let li = document.createElement("li")
+      li.innerText = move_bot
+      
+      ul.append(li)
+
+    
+    move(move_bot)
+    
+
+  })
+
+  
+  
+
+
+
+
+
+//   document.addEventListener('keydown', function(e) {
+//     if (e.key === "ArrowRight") {
+//       console.log("ArrowRight") 
+//     }
+//   })
+
+//   document.addEventListener('keydown', function(e) {
+//     if (e.key === "ArrowUp") {
+//       console.log("ArrowUp") 
+//     }
+//   })
+
+//   document.addEventListener('keydown', function(e) {
+//     if (e.key === "ArrowRight") {
+//       console.log("ArrowRight") 
+//     }
+//   })
 })
